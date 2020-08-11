15953e191fb10dc0b9d9945cb27823d745499ef9
John Zhang <johnhaoxian@gmail.com>
2020-08-11 16:29:44 -0400

    Updates

---
+++ precache-manifest.70297c160d5486f38910c3181806ef6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f90e354a3c44ed30d109fd63eb19cc1d",
+    "url": "/react-playground/index.html"
+  },
+  {
+    "revision": "9819ec3b5290e57529b8",
+    "url": "/react-playground/static/css/main.49a85ea5.chunk.css"
+  },
+  {
+    "revision": "3026e0b1fe2fa9c83a3c",
+    "url": "/react-playground/static/js/2.1dd246d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-playground/static/js/2.1dd246d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9819ec3b5290e57529b8",
+    "url": "/react-playground/static/js/main.81338bf2.chunk.js"
+  },
+  {
+    "revision": "a0384384110ee4cae2cf",
+    "url": "/react-playground/static/js/runtime-main.7942e741.js"
+  }
+]);
